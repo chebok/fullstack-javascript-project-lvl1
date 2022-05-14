493bf710ce49f1dc3051a3b75ba5ef85b7fa2552
chebok <vovakuzmin@gmail.com>
2022-05-14 21:14:16 +0300

    step 9 fix

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 
 function greeting() {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hi ${userName}!`);
+  console.log(`Hello ${userName}!`);
 }
 export default greeting;
